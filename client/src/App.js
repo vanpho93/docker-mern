3f4d0510c36eb880eda74169eb586917347ed16f
Van Pho Nguyen <vanpho02@gmail.com>
2019-01-26 18:22:19 +0700

    form to add user

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import * as axios from 'axios';
 
+const API_ENDPOINT = 'http://localhost:3000';
+
 class App extends Component {
-  state = { users: [], loading: true }
+  state = { users: [], loading: true, name: '', email: '', avatar: '' }
+
   componentDidMount = async () => {
-    const API_ENDPOINT = 'http://localhost:3000';
     const response = await axios.get(API_ENDPOINT + '/user');
-    this.setState({ loading: false, users: response.data.result })
+    this.setState({ loading: false, users: response.data.result });
   }
 
+  addUser = async () => {
+    const { name, email, avatar, users } = this.state;
+    const response = await axios.post(API_ENDPOINT + '/user', { name, email, avatar });
+    this.setState({ users: users.concat(response.data.result) });
+  } 
+
   render() {
-    const { loading, users } = this.state;
+    const { loading, users, name, email, avatar } = this.state;
     if (loading) return <p>Loading...</p>;
     return (
       <div>
+        <div>
+            <h3>Add user</h3>
+            <input value={name} placeholder="name" onChange={evt => this.setState({ name: evt.target.value })} />
+            <input value={email} placeholder="email" onChange={evt => this.setState({ email: evt.target.value })} />
+            <input value={avatar} placeholder="avatar" onChange={evt => this.setState({ avatar: evt.target.value })} />
+            <button onClick={this.addUser}>Add</button>
+          </div>
         {users.map(user => (
           <div key={user._id}>
             <h3>{user.name}</h3>
