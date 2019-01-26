753d1100c9d8f025e6f96a288e390d21a875b4e1
Van Pho Nguyen <vanpho02@gmail.com>
2019-01-26 18:37:12 +0700

    starter finished

--- server/src/connect-database.ts
+++ server/src/connect-database.ts
@@ -3,7 +3,20 @@ import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 
 export async function connectDatabase() {
-    await mongoose.connect(getDatabaseUri(), { useMongoClient: true });
+    let isSuccess = false;
+    let retry = 0;
+    const databaseUri = getDatabaseUri();
+    while (retry++ < 1000) {
+        isSuccess = await mongoose.connect(databaseUri, { useMongoClient: true })
+            .then(() => true)
+            .catch(() => false);
+        if (isSuccess) break;
+        await sleep(3);
+        console.log(`Retry connect to database. Time: ${retry}`);
+    }
+    if (isSuccess) return console.log(`Database connected at ${databaseUri}`);
+    console.log('Cannot connect database ', databaseUri);
+    process.exit(0);
 }
 
 function getDatabaseUri() {
@@ -12,4 +25,10 @@ function getDatabaseUri() {
     if (NODE_ENV === 'docker') return 'mongodb://mongodb/mydb';
     if (NODE_ENV === 'local') return 'mongodb://localhost/mydb';
     return '';
+}
+
+function sleep(seconds: number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, seconds * 1000);
+    });
 }
